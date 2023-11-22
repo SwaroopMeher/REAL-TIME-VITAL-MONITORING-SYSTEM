8c4b663b11817ffb5fe33e72677d49ef747dc4c7
Shriansh Nauriyal <nauriyal.shriansh@gmail.com>
2023-11-21 19:44:11 -0500

    Updated produce vitals script

--- kafka_produce_patient_vitals.py
+++ kafka_produce_patient_vitals.py
@@ -7,7 +7,7 @@ spark = SparkSession.builder.appName("KafkaProducer").getOrCreate()
 
 # Define the schema for the CSV file
 schema = StructType([
-    StructField("customerId", IntegerType(), True),
+    StructField("patientId", IntegerType(), True),
     StructField("heartBeat", FloatType(), True),
     StructField("systolicBP", FloatType(), True),
     StructField("diastolicBP", FloatType(), True),
@@ -17,25 +17,21 @@ schema = StructType([
 ])
 
 # Read data from the CSV file
-df = spark.read.csv("patient_vitals.csv.csv", header=True, schema=schema)
-
-# Transform the data
-df_transformed = df.selectExpr(
-    "customerId",
-    "heartBeat",
-    "systolicBP",
-    "diastolicBP",
-    "temperature",
-    "respirationRate",
-    "spO2"
-)
+df_stream = spark.readStream \
+    .format("csv") \
+    .option("header", True) \
+    .schema(schema) \
+    .option("path", "patient_vitals_CSV/patient_vitals*.csv") \
+    .option("rateLimit", 1) \
+    .load()
 
 # Write the transformed data to a Kafka topic
-df_transformed.writeStream \
+df_stream.writeStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "localhost:9092") \
     .option("topic", "Patients-Vitals-Info") \
     .option("startingOffsets", "earliest") \
+    .option("checkpointLocation", "./kafka_checkpoint/produce_patient_vitals/") \
     .start() \
     .awaitTermination()
 
